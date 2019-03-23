65eecab8f66c8719f7d620d614a4c6cca13fddb9
ziqi <2873103880@qq.com>
2019-03-23 15:41:48 -0500

    just add a wang

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ package ziqi.first;
 
 public class Hello{
     public static void main(String[] args){
-	   System.out.println("Hello, the World! test");
+	   System.out.println("Hello, the World! wang");
 	   for(int i=args.length-1; i>-1; i--) {
 	   System.out.println(i + ":" + args[i]);
 	   }
